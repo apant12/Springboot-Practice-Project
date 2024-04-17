d669cdf7f19c222da1ca92bd326ae1e5f3494052
ayushpant <ayushp.9645@gmail.com>
2024-04-16 21:53:24 -0400

    Built the Crud api for product runs on localhost:8080/api/products

---
+++ ProductDemo/src/main/java/com/example/ProductDemo/Controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.example.ProductDemo.Controller;
+
+
+
+import com.example.ProductDemo.entity.Product;
+import com.example.ProductDemo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        Product product = productRepository.findById(id).orElseThrow(RuntimeException::new);
+        product.setName(productDetails.getName());
+        product.setPrice(productDetails.getPrice());
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
+    }
+}
---
+++ ProductDemo/src/main/java/com/example/ProductDemo/entity/Product.java
@@ -0,0 +1,52 @@
+package com.example.ProductDemo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+    public class Product {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        private double price;
+
+        // Constructors, Getters, and Setters
+        public Product() {
+        }
+
+        public Product(Long id, String name, double price) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+
+        // getters and setters
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+    }
+
